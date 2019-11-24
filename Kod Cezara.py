05b8146d15fa2bee6544554e85286cfe2958a79f
PatiKoszi <patrykkosznik@gmail.com>
2019-11-24 17:16:27 +0100

    kod dziala

--- Kod Cezara.py
+++ Kod Cezara.py
@@ -1,9 +1,32 @@
 
-message = "witaj patryk"
-kod = 3
 
-for i in message:
-    print(ord(i))
+def cezar_message(message, key):
+    encrypted_message = ''
+    for i in message:
+        if i.isalpha():
+            digit = ord(i) + key
+            print(digit, end=' ')
+
+            if i.isupper():
+                if digit > 90:
+                    digit -= 26
+                elif digit < 65:
+                    digit +=26
+
+            else:
+                if digit > 122:
+                    digit -= 26
+                elif digit < 97:
+                    digit += 26
+
+            encrypted_message += chr(digit)
+        else:
+            encrypted_message += i
+    return encrypted_message
+
+
+print("\n Twoja zaszyfrowana wiadomość to: ", cezar_message("Wizytowka", 12))
+
 
 
 
