f36edd27257fe8eb4f95d350462c5b0c6874686d
PatiKoszi <patrykkosznik@gmail.com>
2019-12-10 18:50:40 +0100

    sortowanie babelkowe dodalem funkcje i troche zmienilem

--- sortowanieBabelkowe.py
+++ sortowanieBabelkowe.py
@@ -1,23 +1,14 @@
-
-
-
 def sortowanieBabelkowe(lista):
     for i in range(len(lista)):
-        for j in range(len(lista) - 1 - i):
-            if lista[j] > lista[j + 1]:
-                lista = swap(lista,j,j+1)
-            printLista(lista)
+        for j in range(len(lista)-1-i):
+            if lista[j] > lista[j+1]:
+                swap(lista, j, j+1)
+        print(lista)
 
-def swap(lista, i, j):
-    lista[i], lista[j] = lista[j], lista[i]
-    return lista
-
-def printLista(lista):
-    for x in lista:
-        print(x, end=', ')
-    print(' ')
+def swap(lista, a, b):
+    lista[a], lista[b] = lista[b], lista[a]
 
 
+lista = [6,1,5,8,-5,-2,0]
 
-lista = [6, 1, 8, 9, 12, 3, -1, 0, -69]
 sortowanieBabelkowe(lista)
